65f2cfd3100c3581ff1e47396d65bd8b43f140d4
unil <unil19>
2018-10-29 23:31:15 +0800

    项目初始提交

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Bar from "./Bar.js"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Bar></Bar>
+      </div>
+    );
+  }
+}
+
+export default App;
