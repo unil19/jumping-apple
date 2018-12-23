c09b59b4dd8a0bf51fed16ca42aa2272edc19757
unil <unil19>
2018-12-24 01:17:52 +0800

    添加material-ui主题

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Bar from "./Bar.js"
+import Bar from "./Bar.js";
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
+import cyan from '@material-ui/core/colors/cyan';
+import blue from '@material-ui/core/colors/blue';
+import pink from '@material-ui/core/colors/pink';
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: pink,
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Bar></Bar>
-      </div>
+    	<MuiThemeProvider theme={theme}>
+    		<div className="App">
+		        <Bar></Bar>
+		        <p style={{color:"#666"}}>网站建设中...</p>
+		     </div>
+	   </MuiThemeProvider>
     );
   }
 }
